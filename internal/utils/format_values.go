4369578b999a1f7e4d527ef8182c7f0ddedd9431
pedrobealves <pedrobealves@gmail.com>
2024-01-13 21:44:06 -0300

    Refactor field name formatting and value normalization

--- internal/utils/format_values.go
+++ internal/utils/format_values.go
@@ -8,28 +8,43 @@ import (
 	"github.com/iancoleman/strcase"
 )
 
-func NormalizeColor(color string) string {
-	return strings.TrimPrefix(color, "#")
+// getFieldName converte o nome do campo para snake_case.
+func getFieldName(fieldName string) string {
+	return strcase.ToSnake(fieldName)
 }
 
-func QueryFromObject(properties interface{}) string {
+// normalizeFieldValue normaliza o valor do campo, especialmente se o nome do campo contiver "color".
+func normalizeFieldValue(fieldName, fieldValue string) string {
+	if strings.Contains(strings.ToLower(fieldName), "color") {
+		return NormalizeColor(fieldValue)
+	}
+	return fieldValue
+}
+
+// queryFromObject realiza a iteração sobre os campos da estrutura e formata os dados para uma string de consulta.
+func queryFromObject(val reflect.Value) string {
 	var result []string
 
-	val := reflect.ValueOf(properties)
 	typ := val.Type()
 
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
-		fieldName := strcase.ToSnake(typ.Field(i).Name)
+		fieldName := getFieldName(typ.Field(i).Name)
 		fieldValue := fmt.Sprintf("%v", field.Interface())
-
-		// Se o nome do campo contém "color", normalize o valor
-		if strings.Contains(strings.ToLower(fieldName), "color") {
-			fieldValue = NormalizeColor(fieldValue)
-		}
+		fieldValue = normalizeFieldValue(fieldName, fieldValue)
 
 		result = append(result, fmt.Sprintf("%s=%s", fieldName, fieldValue))
 	}
 
 	return strings.Join(result, "&")
+}
+
+// QueryFromObject gera e retorna uma string de consulta a partir dos campos de uma estrutura.
+func QueryFromObject(properties interface{}) string {
+	val := reflect.ValueOf(properties)
+	return queryFromObject(val)
+}
+
+func NormalizeColor(color string) string {
+	return strings.TrimPrefix(color, "#")
 }
